fb8084aa249e8d0303f38ae58d855f27410182e7
Athul Muralidharan <athulmurali@gmail.com>
2018-05-31 14:42:09 -0400

    BrowserRouter added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CourseManager from "./container/CourseManager";
 import Routing from './examples/Routing';
+import { BrowserRouter } from 'react-router-dom'
+
 
 ReactDOM.render(
+    <BrowserRouter>
     <div className="container-fluid">
         <Routing/>
         <CourseManager/>,
-    </div>,
+    </div>
+    </BrowserRouter>,
 
     document.getElementById('root')
 );
