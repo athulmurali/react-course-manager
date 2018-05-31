8e5357f11d3e07c9ce8c5adbcb38a2a430812bc2
Athul Muralidharan <athulmurali@gmail.com>
2018-05-30 22:32:49 -0400

    Added Course manager component

---
+++ src/CourseManager.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+
+
+export default class CourseManager
+    extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>Course Manager</h1>
+
+            </div>
+        )}}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './hello'
+import CourseManager from "./CourseManager";
 
 ReactDOM.render(
-    <Hello/>,
+    <CourseManager/>,
     document.getElementById('root')
 );
