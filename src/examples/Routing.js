f8ce684e658313a14578d47088fb9918a28c78b5
Athul Muralidharan <athulmurali@gmail.com>
2018-05-31 04:22:41 -0400

    Added react-router-dom to dependecies

---
+++ src/examples/Routing.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+
+ const Routing =()=> <div>Hola</div>
+
+
+export default Routing;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CourseManager from "./container/CourseManager";
+import Routing from './examples/Routing';
 
 ReactDOM.render(
-    <CourseManager/>,
+    <div className="container-fluid">
+        <Routing/>
+        <CourseManager/>,
+    </div>,
+
     document.getElementById('root')
 );
