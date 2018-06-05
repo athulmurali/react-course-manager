c13ca87337431f605ca61862503a5b68c75e4898
Athul Muralidharan <athulmurali@gmail.com>
2018-06-05 19:33:14 -0400

    Fixed className css error in Home.js Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,6 @@ import '../../node_modules/font-awesome/css/font-awesome.min.css';
 const Home = () =><div>
     <h1 className="jumbotron justify-content-center">This is home! And I
 love it</h1>
-    <i class="fas fa-heart-circle"></i>
+    <i className="fas fa-heart-circle"></i>
 </div>
 export default Home;
