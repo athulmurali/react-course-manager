cdc5dace8cf8dfd3bec7adadee2ad438d2064dba
Athul Muralidharan <athulmurali@gmail.com>
2018-06-12 20:20:47 -0400

    Commit before widget list implementation

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-import WidgetList from "./container/WidgetList/WidgetList";
 import Routes from "./Router";
 
-
-
-
-// const PageParam =( match) =><p>{match.params.id}</p>;
 
 ReactDOM.render(<Routes/>, document.getElementById('root')
 
