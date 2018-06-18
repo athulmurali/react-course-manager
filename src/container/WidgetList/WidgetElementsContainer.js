15c58786b264d9108a4c433038bfccf65bfcfc09
Athul Muralidharan <athulmurali@gmail.com>
2018-06-18 04:08:10 -0400

    Refractored Image and styled with addition in style file

--- src/container/WidgetList/WidgetElementsContainer.js
+++ src/container/WidgetList/WidgetElementsContainer.js
@@ -4,6 +4,7 @@ import {DELETE_WIDGET} from "../../constants/index"
 import * as actions from '../../actions/index'
 import * as constants from '../../constants/index'
 import ListContainer from "./ListContainer";
+import ImageContainer from "./ImageContainer";
 
 
 ///////////////////////////////////////// Heading //////////////////////////////////////////////////////////////////////
@@ -150,68 +151,6 @@ const ParaContainer = connect(stateToPropsMapperPara,dispatchToPropsMapperPara)(
 
 ///////////////////////////////////////// Paragraph End ////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////// Image /////////////////////////////////////////////////////////////////////////
-const Image = ({widget, preview, imageTextChanged, imageNameChanged}) => {
-    let inputElem3
-    let nameElem
-    return(
-
-        <body>
-            <div className="container widget-container"
-             style={widgetContainerStyle}>
-            <div className="col-md-12">
-                <div hidden={preview}>
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="form-group">
-                                <input onChange={() => imageTextChanged(widget.id, inputElem3.value)}
-                                       ref={node3 => inputElem3= node3}
-                                       value={widget.text} className="form-control"
-                                /> <br/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="form-group">
-                                <input onChange={() => imageNameChanged(widget.id, nameElem.value)}
-                                       value={widget.name}
-                                       ref={node => nameElem = node} className="form-control"/> <br/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="form-group">
-
-                                <h4>Image Preview</h4>
-                            </div></div></div>   </div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="form-group">
-                            <div className="col-md-2">
-                                <img src={widget.text} alt={widget.text} />
-                            </div>
-                        </div></div></div></div></div>
-        </body>
-    )
-}
-
-const stateToPropsMapperImage = state => ({
-    preview: state.preview
-})
-
-const dispatchToPropsMapperImage = dispatch =>
-    ({
-        imageTextChanged: (widgetId, imageText) =>
-            actions.imageTextChanged(dispatch,widgetId, imageText),
-        imageNameChanged: (widgetId, imageName) =>
-            actions.imageNameChanged(dispatch,widgetId, imageName)
-    })
-
-const ImageContainer = connect(stateToPropsMapperImage,dispatchToPropsMapperImage)(Image)
-
-//////////////////////////////////////// Image End /////////////////////////////////////////////////////////////////////
 
 
 //////////////////////////////////////// Link /////////////////////////////////////////////////////////////////////////
