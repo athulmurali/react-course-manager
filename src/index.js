2cb277386033b8b4ae5b4d70e2e6e5c57fa3e4f8
Athul Muralidharan <athulmurali@gmail.com>
2018-06-18 03:56:40 -0400

    deleted files added for testing widgets

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 
 import Routes from "./Router";
-import WidgetList from "./container/WidgetList/WidgetList";
 
 
 ReactDOM.render(<Routes/>, document.getElementById('root')
